f05b3f5f04e666dce0d5688e89c4995e31e71d91
Matt Boutell <boutell@gmail.com>
2014-08-18 13:33:26 -0400

    Refactored duplicate code

--- src/edu/rosehulman/dicewithfriends/utils/GameUtils.java
+++ src/edu/rosehulman/dicewithfriends/utils/GameUtils.java
@@ -6,39 +6,33 @@ import java.util.List;
 import com.appspot.dice_with_friends.dicewithfriends.model.Game;
 
 public class GameUtils {
-	
+
 	public static final int GAME_SCORE_TO_WIN = 10000;
-	
+
 	public static int getUserRound(Game game) {
-		List<Long> scores;
-		if (game.getCreatorKey().equals(PlayerUtils.getPlayerForUser().getEntityKey())) {
-			scores = game.getCreatorScores();
-		} else {
-			scores = game.getInviteeScores();
-		}
-		if (scores == null) {
-			scores = new ArrayList<Long>();
-		}
-		return scores.size();
-	}	
-	
-	public static Long getUserScore(Game game) {
-		List<Long> scores;
-		if (game.getCreatorKey().equals(PlayerUtils.getPlayerForUser().getEntityKey())) {
-			scores = game.getCreatorScores();
-		} else {
-			scores = game.getInviteeScores();
-		}
-		if (scores == null) {
-			scores = new ArrayList<Long>();
-		}
-		Long sum = 0L;
+		return getScores(game).size();
+	}
+
+	public static long getUserScore(Game game) {
+		List<Long> scores = getScores(game);
+		long sum = 0L;
 		for (Long score : scores) {
 			sum += score;
 		}
 		return sum;
-	}	
-	
+	}
 
-	
+	private static List<Long> getScores(Game game) {
+		List<Long> scores;
+		if (game.getCreatorKey().equals(PlayerUtils.getPlayerForUser().getEntityKey())) {
+			scores = game.getCreatorScores();
+		} else {
+			scores = game.getInviteeScores();
+		}
+		if (scores == null) {
+			scores = new ArrayList<Long>();
+		}
+		return scores;
+	}
+
 }
